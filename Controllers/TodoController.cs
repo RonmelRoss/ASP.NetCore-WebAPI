057239af721f8e57b6457a09ac78c1e4aed2f212
Ronmel Ross <ronmel_ross@ligph.com>
2019-08-16 17:43:48 +0800

    001 - ASP.Net Core Web API

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -41,7 +41,7 @@ namespace WebApplication1.Controllers
         {
             var todoItem = await _context.TodoItems.FindAsync(id);
 
-            if(todoItem == null)
+            if (todoItem == null)
             {
                 return NotFound();
             }
@@ -53,9 +53,42 @@ namespace WebApplication1.Controllers
         [HttpPost]
         public async Task<ActionResult<TodoItem>> PostToDoItem(TodoItem item)
         {
-            _context.TodoItems.Add(item):
-            await _context
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetToDoItem), new { id = item.Id }, item);
         }
 
+        // PUT: api/ToDo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if(id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,8 @@ namespace WebApplication1
                 app.UseHsts();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
